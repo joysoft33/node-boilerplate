25835ee894571b6c51013326363bf6cd24dad20b
Eric LEON <eric@wildcodeschool.fr>
2017-10-17 18:46:03 +0200

    Adding server directory to separate back and front sources

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 'use strict';
 
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const models = require('./models');
-const usersRoutes = require('./routes/users');
+const models = require('./server/models');
+const usersRoutes = require('./server/routes/users');
 
+// Our server listening port
 let port = process.env.NODE_PORT || 3000;
+// Create our express application
 const app = express();
 
+// Accept bodies url and json encoded
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-
+app.use(bodyParser.json());
+  
 // Set static file directory
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, '/public')));
+app.set('views', path.join(__dirname, '/server/views'));
 
 // Serve index file
 app.set('view engine', 'ejs')
